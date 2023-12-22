a8873e504484b4df986f254409505cc6f9504f33
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-12-22 23:18:14 +0200

    added filter by category

--- app/macbookPage/page.tsx
+++ app/macbookPage/page.tsx
@@ -1,3 +1,23 @@
+import macbookArray from "@/utils/macbookArray";
+import ProductsListItem from "../components/products/ProductsListItem";
+
 export default function MacbookPage() {
-  return <h1 className="container mx-auto flex items-center">Macbook</h1>;
+  return (
+    <div className="container mx-auto">
+      <h1 className="container mx-auto flex items-center">Macbook</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {macbookArray.map(({ id, img, title, description, category }) => (
+          <div className="grid items-center border border-gray-400 rounded">
+            <ProductsListItem
+              id={id}
+              img={img}
+              title={title}
+              description={description}
+              category={category}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- app/watchPage/page.tsx
+++ app/watchPage/page.tsx
@@ -1,3 +1,23 @@
+import watchArray from "@/utils/watchArray";
+import ProductsListItem from "../components/products/ProductsListItem";
+
 export default function WatchPage() {
-  return <h1 className="container mx-auto flex items-center">Watch</h1>;
+  return (
+    <div className="container mx-auto">
+      <h1 className="container mx-auto flex items-center">Watch</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {watchArray.map(({ id, img, title, description, category }) => (
+          <div className="grid items-center border border-gray-400 rounded">
+            <ProductsListItem
+              id={id}
+              img={img}
+              title={title}
+              description={description}
+              category={category}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
