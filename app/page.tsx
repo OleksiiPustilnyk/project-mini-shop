8f3a746f17296847039397e1b79661e61df7a97f
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-12-22 22:51:23 +0200

    install SASS, stalized products card and categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import ProductsList from "./components/products/ProductsList";
+import Sidebar from "./components/Sidebar";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <h1 className="container mx-auto flex items-center">Home</h1>
       <div className="container mx-auto grid grid-cols-4 gap-4">
-        <div className=" bg-green-300 h-[fit-content]">
-          <h3>Categories:</h3>
-          <ul className="flex gap-6 list-none  ">
-            <li className="hover:text-sky-400">
-              <Link href="/iphone">iPhone</Link>
-            </li>
-            <li className="hover:text-sky-400">
-              <Link href="/watch">Watch</Link>
-            </li>
-            <li className="hover:text-sky-400">
-              <Link href="/macbook">Macbook</Link>
-            </li>
-          </ul>
-        </div>
-        <div className=" bg-orange-300 col-span-3 min-h-[fit-content]">
+        <Sidebar />
+        <div className="col-span-3 min-h-[fit-content]">
           <ProductsList />
         </div>
       </div>
